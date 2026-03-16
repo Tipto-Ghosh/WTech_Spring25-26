5d9949f50cd40a2aa47e12708a3171fe42d8c501
Tipto-Ghosh <tiptoghosh@gmail.com>
2026-03-16 21:52:19 +0600

    added on input change function and quantity show in console.

--- Lab Performance_MID/JS/Validation.js
+++ Lab Performance_MID/JS/Validation.js
@@ -1,42 +1,13 @@
-console.log("JS file connected.")
+console.log("JS file connected.");
 
-document.addEventListener("DOMContentLoaded", function () {
+function updateTotal(){
     
-    let quantity = document.getElementById("quantity");
-    let totalPrice = document.getElementById("totalPrice");
+    let quantityInput = document.getElementById("quantity");
+    let totalPriceInput = document.getElementById("totalPrice");
     let errorMsg = document.getElementById("errorMsg");
 
-    quantity.addEventListener("input", function () {
-        calculate_price(quantity, totalPrice, errorMsg);
-    });
+    let quantity = parseInt(quantityInput.value) || 0;
+    console.log(quantity);
 
-});
-
-function calculate_price(quantity , totalPrice , errorMsg) {
-    let unit_price = 1000;
-    let days = 30;
-
-    let q = parseInt(quantity.value);
-    if (isNaN(q)) {
-        q = 0;
-    }
-
-    // handle -ve
-    if (q < 0) {
-        errorMsg.textContent = "Quantity cannot be negative!";
-        quantity.value = 0;
-        q = 0;
-    } 
-    else {
-        errorMsg.textContent = "";
-    }
-
-    // calculate total
-    let total = unit_price * q * days;
-    totalPrice.value = total;
-
-    if (total > 1000) {
-        alert("Eligible for a gift coupon");
-    }
-
+    
 }
