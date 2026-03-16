d329974be8ce6ba35a3e624f3c1955384d12651b
Tipto-Ghosh <tiptoghosh@gmail.com>
2026-03-16 21:53:35 +0600

    adding final code for validation.

--- Lab Performance_MID/JS/Validation.js
+++ Lab Performance_MID/JS/Validation.js
@@ -9,5 +9,24 @@ function updateTotal(){
     let quantity = parseInt(quantityInput.value) || 0;
     console.log(quantity);
 
+    if (quantity < 0) {
+        errorMsg.innerHTML = "Quantity can't be negative";
+        errorMsg.style.color = "red";
+
+        quantityInput.value = 0;
+        totalPriceInput.value = 0;
+        return;
+    } else {
+        errorMsg.innerHTML = "";
+    }
+
+    let unit_price = 1000;
+    let days = 30;
     
+    let total = quantity * unit_price * days;
+    totalPriceInput.value = total;
+
+    if (total > 1000) {
+        alert("Eligible for a gift coupon");
+    }
 }
